d935df47696b3ec70187f28afa79e84b1b8654dd
Parkseonho <tjsgh2103@gmail.com>
2022-07-26 23:43:22 +0900

    이미지 업로드

--- sbbb/src/main/java/com/mysite/sbbb/board/BoardService.java
+++ sbbb/src/main/java/com/mysite/sbbb/board/BoardService.java
@@ -1,10 +1,37 @@
 package com.mysite.sbbb.board;
 
-import lombok.RequiredArgsConstructor;
+import com.mysite.sbbb.board.dao.BoardRepository;
+import com.mysite.sbbb.board.domain.Board;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
-@RequiredArgsConstructor
+import java.io.File;
+import java.util.UUID;
+
+
 @Service
 public class BoardService {
 
+    @Autowired
+    private BoardRepository boardRepository;
+
+    public void write(Board board, MultipartFile file)throws Exception{
+        String projectPath = System.getProperty("user.dir") + "\\src\\main\\resources\\static\\files" ;
+
+        UUID uuid = UUID.randomUUID();
+
+        String fileName = uuid + "_" + file.getOriginalFilename();
+
+        File saveFile = new File(projectPath, fileName);
+
+        file.transferTo(saveFile);
+
+        boardRepository.save(board);
+    }
+
+    public Board boardView(Integer id) {
+
+        return boardRepository.findById(id).get();
+    }
 }
--- sbbb/src/main/java/com/mysite/sbbb/board/controller/BoardController.java
+++ sbbb/src/main/java/com/mysite/sbbb/board/controller/BoardController.java
@@ -1,19 +1,44 @@
 package com.mysite.sbbb.board.controller;
 
 import com.mysite.sbbb.board.BoardForm;
+import com.mysite.sbbb.board.BoardService;
+import com.mysite.sbbb.board.domain.Board;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.multipart.MultipartFile;
 
 @RequestMapping("/board")
 @Controller
 public class BoardController {
+    @Autowired
+    private BoardService boardService;
+
     @GetMapping("/page")
     public String boardPage(BoardForm boardForm){
         return "user/boardwrite";
     }
 
+    @PostMapping("/page")
+    public String boardWritepro(Board board, MultipartFile file)throws Exception{
+      boardService.write(board, file);
 
+      return "user/question";
+    }
+
+    @PostMapping("/update/{id}")
+    public String boardUpdate(@PathVariable("id") Integer id, Board board, MultipartFile file) throws Exception{
+        Board boardTemp = boardService.boardView(id);
+        boardTemp.setTitle(board.getTitle());
+        boardTemp.setContent(board.getContent());
+
+        boardService.write(boardTemp, file);
+
+        return "redirect:/question/list";
+    }
 
 
 }
