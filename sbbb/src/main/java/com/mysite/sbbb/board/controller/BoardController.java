011f3f6b4ef7dd04f02af0fc4887f6974f62f44f
Parkseonho <tjsgh2103@gmail.com>
2022-07-27 13:12:51 +0900

    게시글 리스트에 이미지 표시(ì시도)

--- sbbb/src/main/java/com/mysite/sbbb/board/controller/BoardController.java
+++ sbbb/src/main/java/com/mysite/sbbb/board/controller/BoardController.java
@@ -7,10 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 @RequestMapping("/board")
@@ -35,16 +32,13 @@ public class BoardController {
     public String boardPageWrite(Board board, MultipartFile file)throws Exception{
       boardService.write(board, file);
 
-      return "user/board";
+        return "redirect:/board/list";
     }
 
-/*
     @RequestMapping("/detail/{id}")
-    public String showDetail(Model model, @PathVariable("id") Integer id, AnswerForm answerForm){
+    public String showDetail(Model model, @PathVariable("id") Integer id){
         Board board = this.boardService.getBoard(id);
-        boardService.updateHit(id);
         model.addAttribute("board", board);
         return "user/board_detail";
     }
-*/
 }
